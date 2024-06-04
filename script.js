ea271a53b93dc4b60ea7b4191104b035f699bedb
Rylan <159960720+Tboomer2@users.noreply.github.com>
2024-06-04 14:08:41 +0000

    Version 0.1

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ console.log("Script loaded...")
 let badguys = []
 let keyIndex = -1;
 let i = 0
+let move = 1
+let game = true
 // setingup the page does not do much
 function setup(){
   createCanvas(windowWidth,windowHeight-5);
@@ -37,19 +39,25 @@ let player= new default_player(30,30,10,10)
 // Math.floor(Math.random()* windowHeight+1)
 // gives you the ability to shoot the gun does not do much yet
 function shoot(){
+
+}
+
+function mousePressed() {
   i++
   console.log("Bang Bang")
   circle(mouseX , mouseY ,2);
   badguys.push(new bad_guy(Math.floor(Math.random()* windowWidth+1),Math.floor(Math.random()* windowHeight+1)))
   console.log(badguys)
+  for (let l=0;l<badguys.length;l++){
+    if (badguys[l].x<mouseX+15&&badguys[l].x>mouseX-15){
+      if (badguys[l].y<mouseY+15&&badguys[l].y>mouseY-15){
+        badguys.splice(l,1)
+        return
+    }
+  }
+  // circle(Math.floor(Math.random()* windowWidth+1),Math.floor(Math.random()*windowHeight+1),5)
 }
-
-function mousePressed() {
-  shoot()
-  moveBadGuy()
-  circle(Math.floor(Math.random()* windowWidth+1),Math.floor(Math.random()*windowHeight+1),5)
 }
-
 
 // function keyPressed() {
 //   let keyIndex = -1;
@@ -76,39 +84,65 @@ function getdistance(u,o,p,l){
 // this is how all the enemys move about
 function moveBadGuy(){
   for (let i=0;i<badguys.length;i++){
-    newX = badguys[i].x+((Math.random()* 5)-2.5)
-    newY= badguys[i].y+((Math.random()* 5)-2.5)
+    if (badguys[i].x<player.x+10&&badguys[i].x>player.x-10){
+      if (badguys[i].y<player.y+ 10&&badguys[i].y>player.y-10){
+        console.log("DIE DIE DIE")
+        game = false
+        return false
+    }
+  }
+    newX = badguys[i].x+((Math.random()* 4)-2)
+    newY= badguys[i].y+((Math.random()* 4)-2)
     while (getdistance(newX,newY,player.x,player.y)>getdistance(badguys[i].x,badguys[i].y,player.x,player.y)){
       //||(!(getdistance(badguys[i].x,badguys[i].y,mouseX,mouseY)>2)))
-      newX = badguys[i].x+((Math.random()* 5)-2.5)
-      newY= badguys[i].y+((Math.random()* 5)-2.5)
+      newX = badguys[i].x+((Math.random()* 5)-2)
+      newY= badguys[i].y+((Math.random()* 4)-2)
     }
     badguys[i].y=newY
     badguys[i].x=newX
-    circle(badguys[i].x,badguys[i].y,12)
+    circle(badguys[i].x,badguys[i].y,30)
 
   }
+  return true
 }
+
 function draw(){
   let newY
   let newX
+  if (game == false){
+    console.log("end")
+  }
+if (game==true){
   background(124, 252, 0	)
-  circle(player.x,player.y,10)
+  circle(player.x,player.y,20)
   if (keyIsDown(65) === true) {
-    player.x -= 1;
+    player.x -= move;
   }
 
   if (keyIsDown(68) === true) {
-    player.x += 1;
+    player.x += move;
   }
 
   if (keyIsDown(87) === true) {
-    player.y -= 1;
+    player.y -= move;
   }
 
   if (keyIsDown(83) === true) {
-    player.y += 1;
+    player.y += move;
+  }
+  if(player.x-10<0){//off left
+    player.x=player.x+move
+  }
+  if(player.x+10>windowWidth){//off right
+    player.x=player.x-move
+  }
+  if(player.y-10<0){//off top
+    player.y=player.y+move
+  }
+  if(player.y+10>windowHeight){//off bottom
+    player.y=player.y+move
   }
   circle(player.x,player.y,20)
-  moveBadGuy()
+  game=moveBadGuy()
+}
 }
